d23625c279f6796a4041fa251888978a42332de7
Jasika Awasthi <jasikaawasthi@gmail.com>
2026-06-19 20:16:30 +0530

    Update nlp_models.py

--- Fake_Job_Detection/src/models/nlp_models.py
+++ Fake_Job_Detection/src/models/nlp_models.py
@@ -73,20 +73,20 @@ class JobCategoryClassifier:
     }
 
     def __init__(self):
-    self.tfidf = TfidfVectorizer(
-        max_features=10000,
-        ngram_range=(1, 2),
-        sublinear_tf=True,
-        min_df=2
-    )
-
-    self.label_encoder = LabelEncoder()
-
-    # Streamlit Cloud compatible Logistic Regression
-    self.classifier = LogisticRegression(
-        max_iter=1000,
-        random_state=42
-    )
+        self.tfidf = TfidfVectorizer(
+            max_features=10000,
+            ngram_range=(1, 2),
+            sublinear_tf=True,
+            min_df=2
+        )
+    
+        self.label_encoder = LabelEncoder()
+    
+        # Streamlit Cloud compatible Logistic Regression
+        self.classifier = LogisticRegression(
+            max_iter=1000,
+            random_state=42
+        )
 
     self.is_fitted = False
 
